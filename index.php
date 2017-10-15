a26e151824890cf7d68fc9713218f090f1530958
FarhanRasyidK <30396251+FarhanRasyidK@users.noreply.github.com>
2017-10-15 21:48:17 +0700

    tugas pk 5,XI RPL 2 ,16 ,FARHAN RASYI K
    tugas pk 5,XI RPL 2 ,16 ,FARHAN RASYI K

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Form Register</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<div id="form">
+	<div class="content">
+
+		<h1> MASTAH </h1>
+		<h3>Daftar untuk melihat foto dan video <br>dari temen temen mu</h3>
+		<button>masuk menggunakan Facebook</button><br>
+		<p style="margin: 12px; color: gray">Atau</p>
+
+		<form action="save1.php" method="GET">
+		<input type="text" name="email" placeholder="Nomor HP atau Email"><br>
+		<input type="text" name="fullname" placeholder="nama lengkap"><br>
+		<input type="text" name="username" placeholder="nama akun"><br>
+		<input type="password" name="password" placeholder="password"><br>
+		<button type="submit">Sign Up</button><br>
+		</form>
+
+		<p>By signing up, you agree to our<br><b>Terms & Privacy Policy</b>.</p>
+	</div>
+</center>
+</div>
+<center>
+
+</body>
+</html>
